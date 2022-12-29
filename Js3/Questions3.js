04e8153872a81d417a78509b533864b3075c692d
VASUNDHARA PANT <96621003+pantvasu8@users.noreply.github.com>
2022-12-29 16:41:08 +0530

    Add files via upload

---
+++ Js3/Questions3.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The 'Central Rice Research Institute' is located at",
+    answer: "Cuttak",
+    options: [
+      "Ratamundry",
+      "Madras",
+      "Cuttak",
+      "Cochin"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The percentage of irrigated land in India is about",
+    answer: "35%",
+    options: [
+      "45%",
+      "65%",
+      "35%",
+      "25%"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The percentage of Earth's surface covered by India is ",
+    answer: "2.4%",
+    options: [
+      "2.4%",
+      "3.4%",
+      "4.4%",
+      "5.4%"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Three important rivers of Indian subcontinent have their sources near the 'Mansarovar lake' in the great Himalayas these rivers are ",
+    answer: "Bhramputra,Indus & Satluj",
+    options: [
+      "Indus,Jhelum & Satluj",
+      "Bhramputra,Indus & Satluj",
+      "Bhramputra,Indus & Yamuna ",
+      "Jhelum,Satluj & Yamuna"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The oldest refinery in India is at",
+    answer: "Digboi,Assam",
+    options: [
+      "Digboi,Assam",
+      "Haldia,near Assam",
+      "Bayali,near Baroda",
+      "Noonmati,Assam"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
