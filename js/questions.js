621fc0aabcee9d4361dffce12f00e1b9a2008c53
VASUNDHARA PANT <96621003+pantvasu8@users.noreply.github.com>
2022-12-29 19:53:31 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Grand central terminal ,Park Evenue,New York is the world's",
+    answer: "Largest railway station",
+    options: [
+      "Largest railway station",
+      "Highest railway station",
+      "Longest railway station",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The members of Rajya Sabha are elected by",
+    answer: "Elected members of the Legislative Assembly",
+    options: [
+      "The people",
+      "Lok Sabha",
+      "Elected members of the Legislative Assembly",
+      "Elected members of the Legislative Council"
+    ]
+  },
+    {
+    numb: 3,
+    question: "'Dandia' is a popular dance of which state? ",
+    answer: "Gujrat",
+    options: [
+      "Punjab",
+      "Gujrat",
+      "Tamil Nadu",
+      "Maharashtra"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the father of Geometry?",
+    answer: "Euclid",
+    options: [
+      "Aristalle",
+      "Euclid",
+      "Pythogoras",
+      "Kepler"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which country was the 1st non test playing country to beat India in an International match?",
+    answer: "Sri Lanka",
+    options: [
+      "Canada",
+      "Sri Lanka",
+      "Zimbabwe",
+      "East Africa"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
